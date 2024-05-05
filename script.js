29772386bec427198b8c43ef47b600bdf21c022c
TheJumbov12 <114319070+TheJumbov12@users.noreply.github.com>
2024-05-04 21:43:37 -0500

    gg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // A table of strings to display
 var table = ["Hello, world!", "I make funny game", "Splatoon 3 kinda a good game ngl", "Snakes can predict earthquakes", "We spend a year on the toilet in our lifetime.", "Adam T. sucks", "To the window, to the wall.", "The teacher spinner used to be called BoilingBrook spinner, and did not have upgrade."];
-
+var online;
 // A function to get a random string from the table
 function getRandomString() {
   var index = Math.floor(Math.random() * table.length);
@@ -14,3 +14,4 @@ function startTimer() {
   updateBottomText(); // Update the bottom text once at the start
   setInterval(updateBottomText, 5000); // Update the bottom text every 30 seconds
 }
+
