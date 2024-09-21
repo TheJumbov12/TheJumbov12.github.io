8bc21cad90b7c5e4f7c68161619e7dc59935cb0d
TheJumbov12 <114319070+TheJumbov12@users.noreply.github.com>
2024-09-21 18:04:23 -0500

    gg

---
+++ WIP/7th/script.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-img');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Automatic Slide
+setInterval(nextSlide, 3000);
+
+// Initial call to display first slide
+showSlide(currentSlide);
