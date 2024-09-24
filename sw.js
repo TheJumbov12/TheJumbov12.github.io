5ed442e5b6d86a9ad88b134e3977f5c0e4efc67c
TheJumbov12 <114319070+TheJumbov12@users.noreply.github.com>
2024-09-24 10:40:53 -0500

    gg

---
+++ sw.js
@@ -0,0 +1,26 @@
+let cacheName = "core" // Whatever name
+// Pass all assets here
+// This example use a folder named «/core» in the root folder
+// It is mandatory to add an icon (Important for mobile users)
+let filesToCache = [
+  "/",
+  "/offline/index.html",
+  "/offline/no-wifi.png",
+  "/offline/developmentlogowhite.png"
+]
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request)
+    })
+  )
+})
