67cf13c1fe8ecdf79b358d9d9646962027b91875
TheJumbov12 <114319070+TheJumbov12@users.noreply.github.com>
2024-10-04 10:12:55 -0500

    gg

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let cacheName = "core" // Whatever name
+let cacheName = "offline" // Whatever name
 // Pass all assets here
 // This example use a folder named «/core» in the root folder
 // It is mandatory to add an icon (Important for mobile users)
